4119e7b62e9d79478dc1da6f40b45ed24021c1c4
Divya <Divya@gmail.com>
2022-03-14 14:11:15 -0400

    resource block added

--- s3.tf
+++ s3.tf
@@ -1,14 +1,14 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
  
- resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
-  
    versioning {
     enabled = var.versioning
   }
   
   tags = var.tags
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
 }
